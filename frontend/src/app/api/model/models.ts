933eca26e78b2927a2a8105c72a263cff6834a56
George Gale <gmgale@icloud.com>
2024-10-06 09:26:08 +0200

    add frontend gen code

---
+++ frontend/src/app/api/model/models.ts
@@ -0,0 +1,6 @@
+export * from './answer';
+export * from './gameSession';
+export * from './gamesGameIdPlayersPostRequest';
+export * from './leaderboardEntry';
+export * from './player';
+export * from './question';
